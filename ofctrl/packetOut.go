b1c4c25278de6f2036a34be2fe8429a97f8e116c
cl1111 <834980140@qq.com>
2022-06-10 13:13:40 +0800

    Add UDP\ICMP packetOut support

--- ofctrl/packetOut.go
+++ ofctrl/packetOut.go
@@ -21,11 +21,9 @@ import (
 	"math/rand"
 	"net"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/contiv/libOpenflow/openflow13"
 	"github.com/contiv/libOpenflow/protocol"
-	"github.com/contiv/libOpenflow/util"
-
-	log "github.com/Sirupsen/logrus"
 )
 
 type Range [2]uint32
@@ -167,8 +165,8 @@ func SendPacket(sw *OFSwitch, packetOut *PacketOut) error {
 }
 
 func GeneratePacketOutData(p *PacketOut) *protocol.Ethernet {
-	var ethData util.Message
-	var ipData util.Message
+	//var ethData util.Message
+	//var ipData util.Message
 	ethPacket := &protocol.Ethernet{
 		HWDst: p.DstMac,
 		HWSrc: p.SrcMac,
@@ -186,24 +184,24 @@ func GeneratePacketOutData(p *PacketOut) *protocol.Ethernet {
 		}
 		p.Header.TCPHeader.Checksum = p.tcpHeaderChecksum()
 		p.Header.IPHeader.Length = 20 + p.Header.TCPHeader.Len()
-		ipData = p.Header.TCPHeader
+		ipData := p.Header.TCPHeader
 		p.Header.IPHeader.Data = ipData
 	case p.Header.UDPHeader != nil:
 		p.Header.IPHeader.Protocol = protocol.Type_UDP
 		p.Header.UDPHeader.Length = p.Header.UDPHeader.Len()
 		p.Header.UDPHeader.Checksum = p.udpHeaderChecksum()
 		p.Header.IPHeader.Length = 20 + p.Header.UDPHeader.Len()
-		ipData = p.Header.UDPHeader
+		ipData := p.Header.UDPHeader
 		p.Header.IPHeader.Data = ipData
 	case p.Header.ICMPHeader != nil:
 		p.Header.IPHeader.Protocol = protocol.Type_ICMP
 		p.Header.ICMPHeader.Checksum = p.icmpHeaderChecksum()
 		p.Header.IPHeader.Length = 20 + p.Header.ICMPHeader.Len()
-		ipData = p.Header.ICMPHeader
+		ipData := p.Header.ICMPHeader
 		p.Header.IPHeader.Data = ipData
 	}
 	p.Header.IPHeader.Checksum = p.ipHeaderChecksum()
-	ethData = p.Header.IPHeader
+	ethData := p.Header.IPHeader
 	ethPacket.Ethertype = protocol.IPv4_MSG
 	ethPacket.Data = ethData
 
